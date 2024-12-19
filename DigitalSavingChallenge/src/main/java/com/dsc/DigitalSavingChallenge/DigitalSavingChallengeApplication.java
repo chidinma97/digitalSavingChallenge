5792adb38c53685e4a04820c637436e3bd83dc80
Chidinma Idika <chidinma97@yahoo.com>
2024-12-18 19:27:00 -0500

    1st commit

---
+++ DigitalSavingChallenge/src/main/java/com/dsc/DigitalSavingChallenge/DigitalSavingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.dsc.DigitalSavingChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSavingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSavingChallengeApplication.class, args);
+	}
+
+}
---
+++ DigitalSavingChallenge/src/test/java/com/dsc/DigitalSavingChallenge/DigitalSavingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsc.DigitalSavingChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalSavingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
